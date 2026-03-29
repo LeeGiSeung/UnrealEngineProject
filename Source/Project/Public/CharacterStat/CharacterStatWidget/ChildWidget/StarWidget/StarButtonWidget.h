143d46693bf5de6493a639cefab596e1cb7fc1cf
LeeGiSeung <dlrltmd5247@naver.com>
2026-03-29 16:55:29 +0900

    Add Star, Relic Button Widget

---
+++ Source/Project/Public/CharacterStat/CharacterStatWidget/ChildWidget/StarWidget/StarButtonWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterStat/CharacterStatWidget/BaseWidget/BaseWidget.h"
+#include "StarButtonWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UStarButtonWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+	
+};
